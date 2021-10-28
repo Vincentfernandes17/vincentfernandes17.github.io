6a7db2cae1b8d0a411810776a4cc90db78438711
Vincentfernandes17 <vincentfernandes1708@gmail.com>
2021-10-28 18:16:51 +0700

    added error massage

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const form = document.forms["Portofolio-Contact-Form-vincentf"];
 const btnKirim = document.querySelector(".btn-kirim");
 const btnLoading = document.querySelector(".btn-loading");
 const myAlert = document.querySelector(".my-alert");
+const myAlert2 = document.querySelector(".my-alert2");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -19,5 +20,8 @@ form.addEventListener("submit", (e) => {
       myAlert.classList.toggle("d-none");
       form.reset();
     })
-    .catch((error) => console.error("Error!", error.message));
+    .catch((error) => {
+      console.error("Error!", error.message);
+      myAlert2.classList.toggle("d-none");
+    });
 });
