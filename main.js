9a7a48deaf95f9cf1e8d45f4f956a70e163979b4
vincentfernandes17 <vincentfernandes1708@gmail.com>
2021-10-28 16:25:31 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,23 @@
+const scriptURL = "https://script.google.com/macros/s/AKfycbyBEFpVP4AdVTYQLT8nrB5Qo8Ne-dd26G2BaE9K4zCUyWXO3zR4ORq-IjWMAFYsbR8uQw/exec";
+const form = document.forms["Portofolio-Contact-Form-vincentf"];
+const btnKirim = document.querySelector(".btn-kirim");
+const btnLoading = document.querySelector(".btn-loading");
+const myAlert = document.querySelector(".my-alert");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  //tombol loading tampilkan, hilangkan kirim
+  btnLoading.classList.toggle("d-none");
+  btnKirim.classList.toggle("d-none");
+
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
+      //tombol loading hilangkan, hilangkan loading
+      console.log("Success!", response);
+      btnLoading.classList.toggle("d-none");
+      btnKirim.classList.toggle("d-none");
+      myAlert.classList.toggle("d-none");
+      form.reset();
+    })
+    .catch((error) => console.error("Error!", error.message));
+});
