64474db581eeb98e42a23ed9ebdffe0e8a5f26a5
Vincentfernandes17 <vincentfernandes1708@gmail.com>
2022-03-19 23:09:06 +0700

    osis

---
+++ main.js
@@ -0,0 +1,88 @@
+const scriptURL = 'https://script.google.com/macros/s/AKfycbyBEFpVP4AdVTYQLT8nrB5Qo8Ne-dd26G2BaE9K4zCUyWXO3zR4ORq-IjWMAFYsbR8uQw/exec';
+const form = document.forms['Portofolio-Contact-Form-vincentf'];
+const btnKirim = document.querySelector('.btn-kirim');
+const btnLoading = document.querySelector('.btn-loading');
+const myAlert = document.querySelector('.my-alert');
+const myAlert2 = document.querySelector('.my-alert2');
+var name1 = document.getElementById('name1');
+var myNav = document.getElementById('nav1');
+var empt = document.forms['Portofolio-Contact-Form-vincentf']['name'].value;
+const forms = document.querySelectorAll('.needs-validation');
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function() {
+  'use strict';
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const formss = document.querySelectorAll('.needs-validation');
+
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(formss).forEach(function(form) {
+    form.addEventListener(
+      'submit',
+      function(event) {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+      },
+      false
+    );
+  });
+})();
+//var aa = document.getElementsByName("name").value;
+//var bb = document.getElementsByName("email").value;
+//var cc = document.getElementsByName("pesan").value;
+
+window.onscroll = function() {
+  'use strict';
+  if (document.documentElement.scrollTop >= 120) {
+    myNav.classList.add('bg-putih');
+    myNav.classList.remove('bg-transparent');
+  } else {
+    myNav.classList.add('bg-transparent');
+    myNav.classList.remove('bg-putih');
+  }
+};
+
+let home = document.querySelector('#home');
+let about = document.querySelector('#About');
+let resume = document.querySelector('#Portfolio');
+let project = document.querySelector('#Projects');
+let contact = document.querySelector('#Contact');
+
+window.addEventListener('scroll', () => {
+  var windo = window.pageYOffset;
+  if (home.offsetTop - 800 <= windo && about.offsetTop - 100 > windo) {
+    document.querySelector('.home').setAttribute('id', 'active');
+    document.querySelector('.about').removeAttribute('id', 'active');
+    document.querySelector('.resume').removeAttribute('id', 'active');
+    document.querySelector('.project').removeAttribute('id', 'active');
+    document.querySelector('.contact').removeAttribute('id', 'active');
+  } else if (about.offsetTop - 800 <= windo && resume.offsetTop - 500 > windo) {
+    document.querySelector('.home').removeAttribute('id', 'active');
+    document.querySelector('.about').setAttribute('id', 'active');
+    document.querySelector('.resume').removeAttribute('id', 'active');
+    document.querySelector('.project').removeAttribute('id', 'active');
+    document.querySelector('.contact').removeAttribute('id', 'active');
+  } else if (resume.offsetTop - 800 <= windo && project.offsetTop - 500 > windo) {
+    document.querySelector('.home').removeAttribute('id', 'active');
+    document.querySelector('.about').removeAttribute('id', 'active');
+    document.querySelector('.resume').setAttribute('id', 'active');
+    document.querySelector('.project').removeAttribute('id', 'active');
+    document.querySelector('.contact').removeAttribute('id', 'active');
+  } else if (project.offsetTop - 800 <= windo && contact.offsetTop - 300 > windo) {
+    document.querySelector('.home').removeAttribute('id', 'active');
+    document.querySelector('.about').removeAttribute('id', 'active');
+    document.querySelector('.resume').removeAttribute('id', 'active');
+    document.querySelector('.project').setAttribute('id', 'active');
+    document.querySelector('.contact').removeAttribute('id', 'active');
+  } else if (contact.offsetTop - 800 <= windo) {
+    document.querySelector('.home').removeAttribute('id', 'active');
+    document.querySelector('.about').removeAttribute('id', 'active');
+    document.querySelector('.resume').removeAttribute('id', 'active');
+    document.querySelector('.project').removeAttribute('id', 'active');
+    document.querySelector('.contact').setAttribute('id', 'active');
+  }
+});
---
+++ recaptcha.php
@@ -0,0 +1,33 @@
+<?php 
+    if(isset($_POST['ContactButton'])) {
+        $url = "https://www.google.com/recaptcha/api/siteverify";
+        $privateKey = "6LfD3EYdAAAAAIz9H5esinlGJpiool9rZP-3zyW5";
+        $response = file_get_contents($url."?secret=".$privateKey."&response=".$_POST['g-recaptcha-response']."&remoteip=".$_SERVER['REMOTE_ADDR']);
+        $data = json_decode($response);
+        if (isset($data->success) AND $data->success==true) {
+            $error = "Error!";
+            $successMsg = "Recaptcha succes";
+            if ($_POST) {
+                if ($_POST['email'] && filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) === false) {
+                    $error .= "The email is invalid!<br>";
+                }
+                if (!$_POST['email']) {
+                    $error .= "An email address is required!<br>";
+                }
+                if (!$_POST['subject']) {
+                    $error .= "A subject is required!<br>";
+                }
+                if (!$_POST['body']) {
+                    $error .= "Content in the body is required!<br>";
+                }
+                if ($error != "") {
+                    $error = '<div class="alert alert-danger" role="alert"><strong>There is an error with your form!</strong><br>' . $error . '</div>';
+                } else {
+                    
+                }
+            }
+        } else {
+            $captchaFail = '<div class="alert alert-danger" role="alert"><strong>There is an error with your form!</strong><br>reCaptcha Verification Failed, Please Try Again.</div>';
+        }
+    }
+?>
